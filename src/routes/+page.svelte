be56514e24bcf8134c503a4c22fdb85e3142209d
Roland Mackintosh <rolandmackintosh@outlook.com>
2023-06-07 14:30:19 -0700

    prettier fails but the blog is going up

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Under Construction :-)</h1>
+<div>
+    <div>
+        <p>"If Rome was built in a day, we would've hired the same contractor!" - A funny sign at a construction site.</p>
+    </div>
+    
+    <div>
+        <p>This used to be a portfolio page displaying old work from 2021. The page no longer does that or even is. Indeed, it, the blog, is not right now, for an unfinished, under construction blog is not a thing, but the beginning of one.</p>
+        <p>Roland is now building his own blog site using SvelteKit in his spare time because he likes SvelteKit and likes writing.</p>
+    </div>
+</div>
