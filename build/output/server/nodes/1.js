87db20f05d3cd9b056cfeed4ba6b86ddb18365cc
Roland Mackintosh <rolandmackintosh@outlook.com>
2023-06-07 15:29:15 -0700

    get built prod program into commit history so we can deploy

---
+++ build/output/server/nodes/1.js
@@ -0,0 +1,7 @@
+
+
+export const index = 1;
+export const component = async () => (await import('../entries/fallbacks/error.svelte.js')).default;
+export const imports = ["_app/immutable/nodes/1.9c23fca0.js","_app/immutable/chunks/index.6dba6488.js","_app/immutable/chunks/singletons.1329df11.js"];
+export const stylesheets = [];
+export const fonts = [];
